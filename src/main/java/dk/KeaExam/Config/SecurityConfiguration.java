48f6c6b09703b1d53047b04895cc94b21b1fe00b
DESKTOP-QS4LEJ6\jobbe <Ecronfeld@gmail.com>
2017-10-24 12:01:50 +0200

    Login functionality
    Minor bug fixes and Login functionality added

---
+++ src/main/java/dk/KeaExam/Config/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package dk.KeaExam.Config;
+
+import dk.KeaExam.model.MyUserDetailsService;
+import dk.KeaExam.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider
+                = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(getPasswordEncoder());
+        return authProvider;
+    }
+
+    private PasswordEncoder getPasswordEncoder() {
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return true;
+            }
+        };
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("**/secured/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/all");
+    }
+}
---
+++ src/main/java/dk/KeaExam/model/MyUserPrincipal.java
@@ -0,0 +1,54 @@
+package dk.KeaExam.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class MyUserPrincipal implements UserDetails {
+
+    private User user;
+
+    public MyUserPrincipal(final User user) {
+        this.user = user;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        System.out.println(user.getPassword());
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        System.out.println(user.getUsername());
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

5899cd245a2f0821fe092e19d41abac2ca152a9a
ECronfeld <ecronfeld@gmail.com>
2017-10-26 22:15:20 +0200

    More registration
    Email added and thymeleaf displays if username is taken

--- src/main/java/dk/KeaExam/controller/RegistrationController.java
+++ src/main/java/dk/KeaExam/controller/RegistrationController.java
@@ -2,14 +2,22 @@ package dk.KeaExam.controller;
 
 import dk.KeaExam.model.MyUserDetailsService;
 import dk.KeaExam.model.User;
+import dk.KeaExam.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
 
 
 @Controller
 public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
@@ -20,7 +28,14 @@ public class RegistrationController {
         }
 
         @PostMapping("/registration")
-        public String greetingSubmit(@ModelAttribute User greeting) {
+        public String greetingSubmit(@ModelAttribute User greeting, BindingResult bindingResult) {
+            User userExists = (userRepository.findByUsername(greeting.getUsername()));
+            if(userExists != null){
+                bindingResult.rejectValue("username", "error.user", "There is already a user with that username");
+            }
+            if(bindingResult.hasErrors()){
+                return"registration";
+            }
             myUserDetailsService.registerUser(greeting);
             return "/result";
         }
--- src/main/java/dk/KeaExam/model/User.java
+++ src/main/java/dk/KeaExam/model/User.java
@@ -17,12 +17,23 @@ public class User {
     @NotNull
     private String password;
 
+    private String email;
+
     public User() {
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, String email) {
         this.username = username;
         this.password = password;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Long getId() {
